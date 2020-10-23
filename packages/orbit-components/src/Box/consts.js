8ebee36b04af0f6837bbb48b39e7028bcedc37b1
mainframev <vgenaev@gmail.com>
2020-10-23 18:17:50 +0500

    fix(box): fixes after review

--- packages/orbit-components/src/Box/consts.js
+++ packages/orbit-components/src/Box/consts.js
@@ -1,29 +1,156 @@
 // @flow
-export const DISPLAY = ["none", "flex", "inline-flex", "block", "inline", "inline-block"];
+import theme from "../defaultTheme";
 
-export const FLEX_WRAP = ["wrap", "nowrap"];
+export const DISPLAY = {
+  NONE: "none",
+  FLEX: "flex",
+  "INLINE-FLEX": "inline-flex",
+  BLOCK: "block",
+  INLINE: "inline",
+  "INLINE-BLOCK": "inline-block",
+};
 
-export const FLEX_DIRECTION = ["row", "column", "row-reverse", "column-reverse"];
+export const WRAP = { WRAP: "wrap", NOWRAP: "nowrap" };
 
-export const FLEX_SHRINK = [0, 1];
+export const DIRECTION = {
+  ROW: "row",
+  COLUMN: "column",
+  "ROW-REVERSE": "row-reverse",
+  "COLUMN-REVERSE": "column-reverse",
+};
 
-export const FLEX_GROW = [0, 1];
+export const SHRINK = [0, 1];
+
+export const GROW = [0, 1];
 
 export const WIDTH_AND_HEIGHT = {
   FULL: "full",
   AUTO: "auto",
 };
 
-export const POSITION = ["absolute", "relative", "fixed"];
+export const POSITION = { ABSOLUTE: "absolute", RELATIVE: "relative", FIXED: "fixed" };
 
-export const ALIGN_ITEMS = ["absolute", "relative", "fixed"];
+export const ALIGN = { START: "start", END: "end", CENTER: "center", STRETCH: "stretch" };
 
-export const JUSTIFY_CONTENT = ["center", "start", "end", "between", "around"];
+export const JUSTIFY = {
+  CENTER: "center",
+  START: "start",
+  END: "end",
+  BETWEEN: "between",
+  AROUND: "around",
+};
 
-export const TEXT_ALIGN = ["left", "right", "center"];
+export const TEXT_ALIGN = { LEFT: "left", RIGHT: "right", CENTER: "center" };
 
-export const ELEVATION = ["suppressed", "flat", "action", "fixed", "raised", "overlay"];
+// TODO: "suppressed", "flat"
+export const ELEVATION = { ACTION: "action", FIXED: "fixed", RAISED: "raised", OVERLAY: "overlay" };
 
-export const BORDER_RADIUS = ["small", "normal", "large", "radiusCircle"];
+export const BORDER_RADIUS = {
+  SMALL: "small",
+  NORMAL: "normal",
+  LARGE: "large",
+  RADIUSCIRCLE: "radiusCircle",
+};
 
-export const OVERFLOW = ["auto", "hidden", "scroll", "visible"];
+export const OVERFLOW = { AUTO: "auto", HIDDEN: "hidden", SCROLL: "scroll", VISIBLE: "visible" };
+
+export const COLORS = [
+  "productLight",
+  "productLightHover",
+  "productLightActive",
+  "productNormal",
+  "productNormalHover",
+  "productNormalActive",
+  "productDark",
+  "productDarkHover",
+  "productDarkActive",
+  "productDarker",
+  "white",
+  "whiteHover",
+  "whiteActive",
+  "cloudLight",
+  "cloudLightHover",
+  "cloudLightActive",
+  "cloudNormal",
+  "cloudNormalHover",
+  "cloudNormalActive",
+  "cloudDark",
+  "inkLighter",
+  "inkLighterHover",
+  "inkLighterActive",
+  "inkLight",
+  "inkLightHover",
+  "inkLightActive",
+  "inkNormal",
+  "inkNormalHover",
+  "inkNormalActive",
+  "orangeLight",
+  "orangeLightHover",
+  "orangeLightActive",
+  "orangeNormal",
+  "orangeNormalHover",
+  "orangeNormalActive",
+  "orangeDark",
+  "orangeDarkHover",
+  "orangeDarkActive",
+  "orangeDarker",
+  "redLight",
+  "redLightHover",
+  "redLightActive",
+  "redNormal",
+  "redNormalHover",
+  "redNormalActive",
+  "redDark",
+  "redDarkHover",
+  "redDarkActive",
+  "redDarker",
+  "greenLight",
+  "greenLightHover",
+  "greenLightActive",
+  "greenNormal",
+  "greenNormalHover",
+  "greenNormalActive",
+  "greenDark",
+  "greenDarkHover",
+  "greenDarkActive",
+  "greenDarker",
+  "blueLight",
+  "blueLightHover",
+  "blueLightActive",
+  "blueNormal",
+  "blueNormalHover",
+  "blueNormalActive",
+  "blueDark",
+  "blueDarkHover",
+  "blueDarkActive",
+  "blueDarker",
+  "socialFacebook",
+  "socialFacebookHover",
+  "socialFacebookActive",
+];
+
+export const SPACINGS = [
+  "none",
+  "XXXSmall",
+  "XXsmall",
+  "Xsmall",
+  "small",
+  "medium",
+  "large",
+  "Xlarge",
+  "XXlarge",
+  "XXXlarge",
+];
+
+export const TOKENS = {
+  none: null,
+  XXXSmall: theme.orbit.spaceXXXSmall,
+  XXsmall: theme.orbit.spaceXXSmall,
+  Xsmall: theme.orbit.spaceXSmall,
+  small: theme.orbit.spaceSmall,
+  medium: theme.orbit.spaceMedium,
+  large: theme.orbit.spaceLarge,
+  Xlarge: theme.orbit.spaceXLarge,
+  XXlarge: theme.orbit.spaceXXLarge,
+  XXXlarge: theme.orbit.spaceXXXLarge,
+};
